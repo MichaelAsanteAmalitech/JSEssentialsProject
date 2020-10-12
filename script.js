58fa513d9d9fbd59f91be40df60a14ef2141817f
MichaelAsamteAmalitech <michael.asante@amalitech.org>
2020-10-12 00:58:38 +0000

    Adding conditions to the for loop of the set function

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ let div = document.querySelector('#modify');
 
 function set() {
     for(let index = 0; index<elements.length; index++){
-        
+        let cssProperty = elements[ index ].getAttribute('id');
+
+        let cssValue = elements[ index ].value;
+
+        div.style[ cssProperty ] = cssValue;
     }
 }
