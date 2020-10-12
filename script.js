583a5cc5fa1afeed27495cf78c2be42e6c022bf2
MichaelAsamteAmalitech <michael.asante@amalitech.org>
2020-10-12 00:56:23 +0000

    For loop integrated

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ let elements=
 document.getElementsByName('cssProperty');
 
 let div = document.querySelector('#modify');
+
+function set() {
+    for(let index = 0; index<elements.length; index++){
+        
+    }
+}
